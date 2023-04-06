508da9098102d2aa698df86956bb72c152048971
Milywayz <jtjenks@gmail.com>
2023-04-06 09:59:03 -0500

    Added another javascript and added a timer in java

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+let timer = 90
+
+let timerID = setInterval(function(){
+
+    timer -=1
+    console.log(timer);
+
+}, 1000)
+
+// timer -=10
+// clearInterval(timerID)
+
+
+let questions = [{question: "What do you spawn with in a new world in Minecraft?" , answers: ["A Pickaxe" , "An Axe" , "Bread" , "Nothing"], correctAnswer: "Nothing" },
+                {question: "How many hearts of damage do you take when you fall 10 blocks in Minecraft with no protection?" , answers: ["2 Full Hearts" , "1 Full Hearts and a Half" , "3 Full Hearts and a Half" , "3 Full Hearts"], correctAnswer: "3 Full Hearts and a Half" },
+
+
+
+]
