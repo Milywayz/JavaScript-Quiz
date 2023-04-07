fa2849e0087849cd5acad29a9e85ac779496773b
Milywayz <jtjenks@gmail.com>
2023-04-07 00:01:33 -0500

    Added eventlistener and question buttons in java

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
 
+let quizDiv = document.querySelector("#quiz");
+let questionShow = document.querySelector("currentQuestion")
+let questionButton1 = document.querySelector("#answer1");
+let questionButton2 = document.querySelector("#answer2");
+let questionButton3 = document.querySelector("#answer3");
+let questionButton4 = document.querySelector("#answer4");
+// let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 let timer = 90
 
@@ -16,20 +23,43 @@ let timerID = setInterval(function(){
 
 let questions = [{question: "What do you spawn with in a new world in Minecraft?" , answers: ["A Pickaxe" , "An Axe" , "Bread" , "Nothing"], correctAnswer: "Nothing" },
                 {question: "How many hearts of damage do you take when you fall 10 blocks in Minecraft with no protection?" , answers: ["2 Full Hearts" , "1 Full Hearts and a Half" , "3 Full Hearts and a Half" , "3 Full Hearts"], correctAnswer: "3 Full Hearts and a Half" },
+                {question: "What is Minecraft's rarest biome?" , answers: ["Ice Spikes Biome" , "Bamboo Jungle Biome" , "Mesa Biome" , "Mushroom Fields Biome"], correctAnswer: "Mushroom Fields Biome" },
 
 ]
 
 currentQuestion = 0
-
-
+renderQuestion();
 
 function renderQuestion(){
 
-    questions[currentQuestion].question
-    questions[currentQuestion].answers[0]
-    questions[currentQuestion].answers[1]
-    questions[currentQuestion].answers[2]
-    questions[currentQuestion].answers[3]
+    questionShow.textContent = questions[currentQuestion].question
+    questionButton1.textContent = questions[currentQuestion].answers[0];
+    questionButton2.textContent = questions[currentQuestion].answers[1]
+    questionButton3.textContent = questions[currentQuestion].answers[2]
+    questionButton4.textContent = questions[currentQuestion].answers[3]
     console.log("correct answer:" + questions[currentQuestion].correctAnswer)
 
 }
+
+
+quizDiv.addEventListener("click" , function(event){
+
+    if(event.target.matches("button")){
+        console.log("clicked!")
+        console.log("value:" +event.target.innerText);
+        console.log("correct answer:" +  questions[currentQuestion].correctAnswer);
+        currentQuestion++
+        renderQuestion();
+
+
+    }
+
+})
+
+
+
+
+
+// some button that saves
+
+// localStorage.setItem("highScores" , JSON.stringify([{"initials": "dre", "score": 80}]))
