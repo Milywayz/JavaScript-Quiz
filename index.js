7ac8fa2496290cb17199f5c8986921f913db178f
Milywayz <jtjenks@gmail.com>
2023-04-06 23:16:11 -0500

    added question render in java

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ let timerID = setInterval(function(){
 let questions = [{question: "What do you spawn with in a new world in Minecraft?" , answers: ["A Pickaxe" , "An Axe" , "Bread" , "Nothing"], correctAnswer: "Nothing" },
                 {question: "How many hearts of damage do you take when you fall 10 blocks in Minecraft with no protection?" , answers: ["2 Full Hearts" , "1 Full Hearts and a Half" , "3 Full Hearts and a Half" , "3 Full Hearts"], correctAnswer: "3 Full Hearts and a Half" },
 
-
-
 ]
+
+currentQuestion = 0
+
+
+
+function renderQuestion(){
+
+    questions[currentQuestion].question
+    questions[currentQuestion].answers[0]
+    questions[currentQuestion].answers[1]
+    questions[currentQuestion].answers[2]
+    questions[currentQuestion].answers[3]
+    console.log("correct answer:" + questions[currentQuestion].correctAnswer)
+
+}
